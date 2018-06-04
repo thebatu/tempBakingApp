b89392db39a366c7ee278641baeb8ea2f9ddec23
thebatu <thebatu@gmail.com>
2018-06-04 11:07:52 +0200

    before assets addition

--- app/src/main/java/com/example/bats/bakingapp/Models/Fragments/IngredientsStepsFragment.java
+++ app/src/main/java/com/example/bats/bakingapp/Models/Fragments/IngredientsStepsFragment.java
@@ -2,19 +2,21 @@ package com.example.bats.bakingapp.Models.Fragments;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
-import com.example.bats.bakingapp.R;
-
-/**
- * Created by bats on 6/1/18.
- *
- */
+import android.support.v4.app.FragmentPagerAdapter;
 
 public class IngredientsStepsFragment extends Fragment{
+
+    private ViewPager mPager;
+    private PagerAdapter myPagerAdapter;
+
 
 
     @Nullable
@@ -22,10 +24,55 @@ public class IngredientsStepsFragment extends Fragment{
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.ingredients_steps_fragment, container, false);
 
+
+//        context = inflater.getContext();
+
+        // Instantiate a ViewPager and a PagerAdapter.
+        mPager = (ViewPager) view.findViewById(R.id.pager);
+
+        // Instantiate a ViewPager and a PagerAdapter.
+       // mPager.setAdapter(new myPagerAdapter(getActivity().getSupportFragmentManager()));
+
+
+        TabLayout tabLayout = (TabLayout) view.findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(mPager);
+
+
         return view;
 
 
     }
+
+//    private class myPagerAdapter extends FragmentPagerAdapter {
+//
+//        public myPagerAdapter(FragmentManager fm){
+//            super(fm);
+//        }
+//
+//        @Override
+//        public Fragment getItem(int position) {
+//            FirstPagerFragment firstFragment = FirstPagerFragment.newInstance();
+//            firstFragment.setPagerNavigationInterface(ScreenSlidePagerActivity.this);
+//
+//            SecondPagerFragment secondFragment = SecondPagerFragment.newInstance();
+//            secondFragment.setPagerNavigationInterface(ScreenSlidePagerActivity.this);
+//
+//
+//            switch (position) {
+//                case 0:
+//                    return firstFragment;
+//                case 1:
+//                    return secondFragment;
+//                default:
+//                    return firstFragment;
+//            }
+//        }
+//
+//        @Override
+//        public int getCount() {
+//            return 3;
+//        }
+//    }
 
 
 
